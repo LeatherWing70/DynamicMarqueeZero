f646f72a69aa01074b141de5fb2be44bf1d101ee
Leather Wing <84696172+LeatherWing70@users.noreply.github.com>
2024-02-10 01:09:18 -0600

    Create systemsel.sh

---
+++ RetroPie/emulationstation/scripts/system-select/systemsel.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -f
+
+script_name=$(basename $0)
+for pid in $(pidof -x $script_name); do
+    if [ $pid != $$ ]; then
+        kill -9 $pid
+    fi 
+done
+
+if [ -f /home/pi/RetroPie/roms/$1/marquee/marquee.png ]; then
+    SOURCE=/home/pi/RetroPie/roms/$1/marquee/marquee.png
+	CACHE=$1/marquee.png
+	EMU=$1
+else
+	SOURCE=""
+	CACHE="retropie.png"
+	EMU=""
+fi
+	
+(sleep .3 && ssh pi@marquee.local "/home/marquee/t.sh \""$CACHE"\" \""$SOURCE"\"" $EMU)&
