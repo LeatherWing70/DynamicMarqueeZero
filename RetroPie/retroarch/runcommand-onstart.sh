81631ff80eeebfcfda900c7690ee062fa18759d1
Leather Wing <84696172+LeatherWing70@users.noreply.github.com>
2024-02-10 11:07:24 -0600

    Update runcommand-onstart.sh

--- RetroPie/retroarch/runcommand-onstart.sh
+++ RetroPie/retroarch/runcommand-onstart.sh
@@ -6,13 +6,13 @@ GAME=${FILE%.*}
 #if Maquee exists on RetroPie
 if [ -f $DIR/marquee/$GAME.png ]
 then
-	ssh pi@marquee.local "/home/marquee/t.sh "$1"/"$GAME".png" $DIR"/marquee/"$GAME".png" $1
+	ssh pi@marquee.local "/home/pi/t.sh "$1"/"$GAME".png" $DIR"/marquee/"$GAME".png" $1
 # else default marquee
 else 
 	if [ -f $DIR/marquee/marquee.png ]
 	then
-		ssh pi@marquee.local "/home/marquee/t.sh "$1"/"$GAME".png" $DIR"/marquee/marquee.png" $1
+		ssh pi@marquee.local "/home/pi/t.sh "$1"/"$GAME".png" $DIR"/marquee/marquee.png" $1
 	else
-		ssh pi@marquee.local "/home/marquee/t.sh retropie.png"
+		ssh pi@marquee.local "/home/pi/t.sh retropie.png"
 	fi
 fi
