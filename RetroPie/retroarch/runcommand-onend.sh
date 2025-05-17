1fb1f13b0fb7817d427d293fd36a01320cb21666
Leather Wing <84696172+LeatherWing70@users.noreply.github.com>
2025-05-16 22:29:38 -0500

    Update runcommand-onend.sh

--- RetroPie/retroarch/runcommand-onend.sh
+++ RetroPie/retroarch/runcommand-onend.sh
@@ -4,7 +4,7 @@ DIR="$(dirname "${3}")"
 
 if [ -f $DIR/marquee/marquee.png ]
 then
-	ssh pi@marquee.local "/home/pi/t.sh '$1'/marquee.png" $DIR"/marquee/marquee.png" $1
+	ssh USERNAME@HOSTNAME "echo '$1'/marquee.png::" $DIR"/marquee/marquee.png::" $1"> /tmp/display.pipe"
 else
-	ssh pi@marquee.local "/home/pi/t.sh retropie.png"
+	ssh USERNAME@HOSTNAME "echo retropie.png > /tmp/display.pipe"
 fi
